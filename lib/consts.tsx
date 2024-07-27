f8abcd451cdf306e6d348ee058127e163457e8f7
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-27 11:47:41 -0300

    I replace magic number with named const.

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -3,3 +3,4 @@ import { zora } from "viem/chains";
 export const CHAIN = zora;
 export const CHAIN_ID = zora.id;
 export const NEWTRO_ARTS_ADDRESS = "0x33912a0d6bEFf5Fb8e5B70688CE858D5e7E8104E";
+export const NEWTRO_FIRST_ZORA_BLOCK = BigInt(2671653);
--- lib/zora/getSetupNewContractLogs.tsx
+++ lib/zora/getSetupNewContractLogs.tsx
@@ -1,4 +1,8 @@
-import { CHAIN_ID, NEWTRO_ARTS_ADDRESS } from "../consts";
+import {
+  CHAIN_ID,
+  NEWTRO_ARTS_ADDRESS,
+  NEWTRO_FIRST_ZORA_BLOCK,
+} from "../consts";
 import {
   zoraCreator1155FactoryImplAddress,
   zoraCreator1155FactoryImplABI,
@@ -6,7 +10,7 @@ import {
 import { getPublicClient } from "../viem";
 
 const getSetupNewContractLogs = async () => {
-  const fromBlock = BigInt(2671653);
+  const fromBlock = NEWTRO_FIRST_ZORA_BLOCK;
   const publicClient = getPublicClient(CHAIN_ID);
   const contractEvents = await publicClient.getContractEvents({
     address: zoraCreator1155FactoryImplAddress[CHAIN_ID],
