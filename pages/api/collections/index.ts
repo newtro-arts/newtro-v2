612b6241fc2f62149ecd3671ad50b31f55bc242b
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-27 11:52:17 -0300

    ping

--- pages/api/collections/index.ts
+++ pages/api/collections/index.ts
@@ -4,6 +4,7 @@ import { formatLogs } from './formatLogs';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const logs = await getSetupNewContractLogs()
+    console.log(logs)
     const formattedLogs = formatLogs(logs);
     res.status(200).json({data: formattedLogs}); 
 }
