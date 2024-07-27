4d48fb870904c65dadb9ddf1378cda15869ee36b
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-27 11:46:03 -0300

    I remove hard-coded RPC url.

--- lib/viem/viem.tsx
+++ lib/viem/viem.tsx
@@ -5,7 +5,7 @@ export const getPublicClient = (chainId: number) => {
   const chain = getViemNetwork(chainId);
   let publicClient = createPublicClient({
     chain,
-    transport: http("https://rpc.zora.energy"),
+    transport: http(),
   }) as any;
   return publicClient as PublicClient;
 };
