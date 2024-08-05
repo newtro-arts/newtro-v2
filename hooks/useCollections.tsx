fa39bbfa0aaee815f42e77a170a8441a563b0e50
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-05 12:37:25 -0300

    I update /archive page to include <CollaborativeCollections /> from Newtro.eth onchain.

--- hooks/useCollections.tsx
+++ hooks/useCollections.tsx
@@ -1,23 +1,14 @@
+import fetchCollectionsWithMetadata from "@/lib/fetchCollectionsWithMetadata";
 import { useState, useEffect } from "react";
 
 const useCollections = () => {
-  const [collections, setCollections] = useState([]);
+  const [collections, setCollections] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchCollections = async () => {
-      try {
-        const response = await fetch("/api/collections");
-        if (!response.ok) {
-          throw new Error("Failed to fetch collections");
-        }
-        const data = await response.json();
-        // TODO: fetch each contractURI from the data.data[i].args.contractURI
-        // TODO: add metadata property to collections array
-
-        setCollections(data.data);
-      } catch (error) {
-        console.error("Error fetching collections:", error);
-      }
+      const collectionsWithMetadata = await fetchCollectionsWithMetadata();
+      if (!collectionsWithMetadata) return;
+      setCollections(collectionsWithMetadata.reverse());
     };
 
     fetchCollections();
