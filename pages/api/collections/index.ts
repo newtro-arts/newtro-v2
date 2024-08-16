ea96cc0a2908af9b73f2462dfb69c74bb6954219
codesage <laughingsagecoder@gmail.com>
2024-08-16 16:47:16 +0200

    creating global chain data formatter

--- pages/api/collections/index.ts
+++ pages/api/collections/index.ts
@@ -1,11 +1,11 @@
 import getSetupNewContractLogs from '@/lib/zora/getSetupNewContractLogs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { formatLogs } from './formatLogs';
+import format from '@/lib/format';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const logs = await getSetupNewContractLogs()
-        const formattedLogs = formatLogs(logs);
+        const formattedLogs = format(logs);
         res.status(200).json({data: formattedLogs});     
     } catch (error) {
         console.error(error);
