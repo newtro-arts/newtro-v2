668a02427bef4f140c95d8919fdaa385f439429b
codesage <laughingsagecoder@gmail.com>
2024-08-16 16:47:38 +0200

    creating the api/tokens route

---
+++ lib/arrayPagination.ts
@@ -0,0 +1,11 @@
+export default function arrayPagination(query: any) {
+  const MAX_LIMIT = 100;
+  let limit = 15, offset = 0;
+
+  if (parseInt(query?.limit) > 0) limit = parseInt(query.limit);
+  if (parseInt(query?.offset) >= 0) offset = parseInt(query.offset);
+
+  limit = limit > MAX_LIMIT ? MAX_LIMIT : limit;
+
+  return { offset, limit };
+}
---
+++ pages/api/tokens/index.ts
@@ -0,0 +1,31 @@
+import arrayPagination from '@/lib/arrayPagination';
+import getCollectorClient from '@/lib/zora/getCollectorClient';
+import getSetupNewContractLogs from '@/lib/zora/getSetupNewContractLogs';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import formatCollectionTokens from './formatCollectionTokens';
+import getTokenMetdata from './getTokenMetadata';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const logs = await getSetupNewContractLogs();
+    const collections = logs.map(({ args }) => args.newContract).filter(addr => !!addr);
+
+    const collectorClient = getCollectorClient();
+    const collectionTokens = await Promise.all(
+      collections.map(addr => collectorClient.getTokensOfContract({ tokenContract: addr }))
+    );
+
+    const allTokens = formatCollectionTokens(collectionTokens);
+    const count = allTokens.length;
+
+    const { limit, offset } = arrayPagination(req.query);
+    const tokens = await getTokenMetdata(allTokens.splice(offset, limit));
+
+    res.status(200).json({ count, tokens });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+}
+
+export default handler;
