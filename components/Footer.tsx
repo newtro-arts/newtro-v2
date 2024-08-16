a94b6e7d29084e66d32881b3e2f8de6a25fa9ff6
1TB <florencgmz@gmail.com>
2024-08-16 17:05:20 -0300

    Modified Mobile footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,49 +1,55 @@
 import React from "react";
-import logo from "../public/Logo_NegroVerde.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { GoLinkExternal } from "react-icons/go";
+import { FaXTwitter, FaInstagram, FaYoutube } from "react-icons/fa6";
+import logo from "../public/Logo_NegroVerde.svg";
+
+interface FooterLinkProps {
+  href: string;
+  label: string;
+}
+
+const FooterLink: React.FC<FooterLinkProps> = ({ href, label }) => {
+  const renderIcon = () => {
+    switch (label.toLowerCase()) {
+      case "x":
+        return <FaXTwitter className="block lg:hidden" size={22} />;
+      case "instagram":
+        return <FaInstagram className="block lg:hidden" size={22}/>;
+      case "youtube":
+        return <FaYoutube className="block lg:hidden" size={22} />;
+      case "warpcast":
+        return <Image src="/warpcast.svg" width={22} height={22} alt="Warpcast" className=" block lg:hidden object-cover" />;  
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Link href={href} target="_blank">
+      <div className="flex items-center lg:items-end px-8 h-full">
+        <GoLinkExternal size={18} className="hidden lg:block" />
+        <p className="ml-2 hidden lg:block">{label}</p>
+        {renderIcon()}
+      </div>
+    </Link>
+  );
+};
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row justify-between pl-5 pr-8 py-1 w-full bottom-0 items-center bg-primary-dark text-fourth-green ">
-      <Link href="/">
+    <footer className="flex flex-col lg:flex-row justify-between lg:pl-5 py-4 lg:py-1 max-w-full bg-primary-dark text-fourth-green border-y border-fourth-green lg:border-none">
+      <Link href="/" aria-label="Home" className="hidden lg:flex">
         <Image alt="Newtro Logo" width={49} height={49} src={logo} />
       </Link>
-      <div className="hidden lg:flex flex-col lg:flex-row items-center justify-end gap-8">
-        <Link
-          href="https://x.com/NewtroArts"
-          target="_blank"
-          className="flex items-center"
-        >
-          <GoLinkExternal size={18} />
-          <p className="ml-2">X</p>
-        </Link>
-        <Link
-          href="https://www.instagram.com/newtroarts/"
-          target="_blank"
-          className="flex items-center"
-        >
-          <GoLinkExternal size={18} />
-          <p className="ml-2">Instagram</p>
-        </Link>
-        <Link
-          href="https://www.youtube.com/channel/UCnr-Jd3qkb0N_e0Dvzhig7Q"
-          target="_blank"
-          className="flex items-center"
-        >
-          <GoLinkExternal size={18} />
-          <p className="ml-2">Youtube</p>
-        </Link>
-        <Link
-          href="https://warpcast.com/newtroarts"
-          target="_blank"
-          className="flex items-center "
-        >
-          <GoLinkExternal size={18} />
-          <p className="ml-2">Warpcast</p>
-        </Link>
+      <div className="flex lg:items-center justify-between lg:justify-end w-full py-4">
+        <FooterLink href="https://x.com/NewtroArts" label="X" />
+        <FooterLink href="https://www.instagram.com/newtroarts/" label="Instagram" />
+        <FooterLink href="https://www.youtube.com/channel/UCnr-Jd3qkb0N_e0Dvzhig7Q" label="Youtube" />
+        <FooterLink href="https://warpcast.com/newtroarts" label="Warpcast" />
       </div>
+      <p className="pragmatica-text uppercase pt-4 text-xs block lg:hidden text-center border-t border-fourth-green">Newtro Arts 2024</p>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -60,7 +60,7 @@ export default function Header() {
       <div
         className={
           nav
-            ? "sm:hidden fixed -top-4 left-0 right-0 bottom-0 flex items-center w-full h-screen bg-primary-dark ease-in duration-300"
+            ? "sm:hidden fixed top-4 left-0 right-0 bottom-0 flex items-center w-full h-screen bg-primary-dark ease-in duration-300"
             : "sm:hidden fixed top-0 left-[-100%] right-0 bottom-0 flex items-center w-full h-screen bg-primary-dark ease-in duration-300"
         }
       >
