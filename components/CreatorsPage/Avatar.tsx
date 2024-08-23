949b12ef8d80737368a14bbc3e794eeba1f11652
codesage <laughingsagecoder@gmail.com>
2024-08-23 20:25:49 +0200

    using 'display_name' for creators name

--- components/CreatorsPage/Avatar.tsx
+++ components/CreatorsPage/Avatar.tsx
@@ -8,7 +8,8 @@ const Avatar = ({ creator }: any) => {
   return (
     <Link
       className='relative aspect-square bg-secondary-white border border-fourth-green rounded-tl-cards rounded-br-cards overflow-hidden'
-      href={`https://zora.co/@${creator.user_profile.username}`}>
+      href={`https://zora.co/@${creator.user_profile.username}`}
+      target="_blank">
       <img
         alt='pfp'
         src={avatar ? getIpfsLink(avatar) : '/default-avatar.png'}
@@ -17,7 +18,7 @@ const Avatar = ({ creator }: any) => {
       <div className='absolute inset-0 flex opacity-0 cursor-pointer hover:opacity-85 transition duration-700 bg-fifth-purple'>
         <div className="flex flex-col justify-end w-full p-4">
           <p className="pragmatica-text text-fourth-green text-sm uppercase overflow-hidden text-ellipsis whitespace-nowrap md:text-base">
-            {creator.user_profile.username}
+            {creator.user_profile.display_name}
           </p>
         </div>
       </div>
