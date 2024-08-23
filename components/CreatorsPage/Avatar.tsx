680e674ae4d3f88cc69b6b242ba239341ee89f09
codesage <laughingsagecoder@gmail.com>
2024-08-23 20:28:34 +0200

    centering creator name

--- components/CreatorsPage/Avatar.tsx
+++ components/CreatorsPage/Avatar.tsx
@@ -17,7 +17,7 @@ const Avatar = ({ creator }: any) => {
       />
       <div className='absolute inset-0 flex opacity-0 cursor-pointer hover:opacity-85 transition duration-700 bg-fifth-purple'>
         <div className="flex flex-col justify-end w-full p-4">
-          <p className="pragmatica-text text-fourth-green text-sm uppercase overflow-hidden text-ellipsis whitespace-nowrap md:text-base">
+          <p className="text-center pragmatica-text text-fourth-green text-sm uppercase overflow-hidden text-ellipsis whitespace-nowrap md:text-base">
             {creator.user_profile.display_name}
           </p>
         </div>
