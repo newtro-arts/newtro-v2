c9617f42ddc00dc092b48c2b83c9331d9bcb55f7
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-25 09:55:02 -0300

    I add RPC for Arbitrum.

--- lib/viem/viem.tsx
+++ lib/viem/viem.tsx
@@ -1,14 +1,16 @@
 import { PublicClient, createPublicClient, http } from "viem";
 import getViemNetwork from "./getViemNetwork";
-import { zora } from "viem/chains";
+import { arbitrum, zora } from "viem/chains";
 
 export const getPublicClient = (chainId: number) => {
   const chain = getViemNetwork(chainId);
+  const isZora = chainId === zora.id;
+  const isArbitrum = chainId === arbitrum.id;
+  let RPC = isZora ? "https://rpc.zora.energy" : undefined;
+  if (isArbitrum) RPC = "https://arbitrum.llamarpc.com";
   let publicClient = createPublicClient({
     chain,
-    transport: http(
-      chainId === zora.id ? "https://rpc.zora.energy" : undefined
-    ),
+    transport: http(RPC),
   }) as any;
   return publicClient as PublicClient;
 };
